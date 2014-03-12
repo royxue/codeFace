e022e056035871252a572bb588f06eb07611819b
Roy Xue <xljroy@gmail.com>
2014-03-12 13:23:49 +0800

    Upload codeFace

---
+++ codeFace.py
@@ -0,0 +1,33 @@
+import sys
+from PIL import Image
+
+HEIGHT =  70
+chars = "   ...',;:clodxkO0KXNWMMM"
+
+def pic2ascii(filename):
+    output = ''
+    image = Image.open(filename)
+    size = getsize(image)
+    image = image.resize(size)
+    image = image.convert('L')
+    pixs = image.load()
+    for y in range(size[1]):
+        for x in range(size[0]):
+            output += chars[(pixs[x,y]/10)-1]
+        output += '\n'
+    print output
+
+def getsize(image):
+    '''Calculate the target picture size
+    '''
+    s_width = image.size[0]
+    s_height = image.size[1]
+    t_height = HEIGHT
+    t_width = (t_height*s_width)/s_height
+    t_width = int(t_width * 2.3)
+    t_size = (t_width ,t_height)
+    return t_size
+
+    
+
+pic2ascii("10.jpg")
